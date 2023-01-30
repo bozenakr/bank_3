1a7d9395f703484d0a4e59920aaea2fd54c1d489
bozenakr <bozena.krasevic@gmail.com>
2023-01-30 13:26:42 +0200

    error msg

--- resources/views/layouts/messages.blade.php
+++ resources/views/layouts/messages.blade.php
@@ -1,30 +1,31 @@
-{{-- <div class="container"> --}}
-<div class="hide" style="with: 100%">
-    @if($errors)
-    @foreach ($errors->all() as $message)
-    {{-- <div class="col-6"> --}}
-    <div class="alert alert-danger m-4 alert-dismissible fade show" role="alert">
-        {{ $message }}
-        {{-- </div> --}}
+<div class="container">
+    <div class="hide" style="with: 100%">
+        @if($errors)
+        @foreach ($errors->all() as $message)
+        <div class="alert alert-danger m-4 alert-dismissible fade show" role="alert">
+            {{ $message }}
+        </div>
     </div>
     @endforeach
     @endif
 
     @if(Session::has('ok'))
-    <div class="hide" style="with: 100%">
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ Session::get('ok') }}
+    <div class="container">
+        <div class="hide" style="with: 100%">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ Session::get('ok') }}
+            </div>
         </div>
     </div>
     @endif
 
     @if(Session::has('no'))
-    <div class="col-6">
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ Session::get('no') }}
+    <div class="container">
+        <div class="hide" style="with: 100%">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ Session::get('no') }}
+            </div>
         </div>
     </div>
     @endif
-
-</div>
 </div>
